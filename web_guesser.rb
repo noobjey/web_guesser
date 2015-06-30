19a424073c5f025391b90ef60f7e1f8fa4eb5b6d
Jason Wright <noobjey@gmail.com>
2015-06-29 20:11:56 -0600

    I1: generates a random number

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 secret_number = Random.rand(100)
 
 get '/' do
-  "The secret number is #{secret_number}"
+  "The secret number is #{secret_number}".upcase
 end
